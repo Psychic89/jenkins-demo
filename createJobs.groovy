bac95778d0586e144fbf8427f930317c380c249e
Robert <robertmutua89@yahoo.com>
2022-01-04 17:46:42 +0100

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/Psychic89/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
